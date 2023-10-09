d07e235f592371cd555832c7712d8a4ada2d0d50
Gabriel Lima <gabriellimagomes.14@hotmail.com>
2023-10-09 18:50:35 -0300

    criação endpoints

---
+++ src/TechLanches/TechLanches.API/Endpoints/ClienteEndpoints.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechLanches.Domain.Services;
+
+namespace TechLanches.API.Endpoints;
+
+public static class ClienteEndpoints
+{
+    public static void MapClienteEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/clientes/{cpf}", BuscarClientePorCpf);
+        app.MapPost("api/clientes", CadastrarCliente);
+    }
+
+    private static async Task<IResult> BuscarClientePorCpf(
+        [FromQuery] string cpf,
+        [FromServices] IClienteService clienteService)
+    {
+        var cliente = await clienteService.BuscarPorCpf(cpf);
+
+        return cliente is null 
+            ? Results.Ok(cliente) 
+            : Results.NotFound(cpf);
+    }
+
+    private static async Task<IResult> CadastrarCliente(
+        string nome,
+        string email,
+        string cpf,
+        [FromServices] IClienteService clienteService)
+    {
+        var cliente = await clienteService.Cadastrar(nome, email, cpf);
+
+        return cliente is null 
+            ? Results.Ok(cliente) 
+            : Results.BadRequest(new { nome, email, cpf });
+    }
+}
--- src/TechLanches/TechLanches.API/Program.cs
+++ src/TechLanches/TechLanches.API/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using TechLanches.API.Endpoints;
+using TechLanches.Application;
+using TechLanches.Domain.Repositories;
+using TechLanches.Domain.Services;
 using TechLanches.Infrastructure;
+using TechLanches.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +13,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<TechLanchesDbContext>(config => config.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<TechLanchesDbContext>(config => 
+    config.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IClienteService, ClienteService>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 
 var app = builder.Build();
 
@@ -19,28 +28,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapClienteEndpoints();
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
