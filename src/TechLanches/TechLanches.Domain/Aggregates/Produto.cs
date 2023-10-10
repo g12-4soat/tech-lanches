766ac4eeaa7c47d7c564915b65917813b77a3f95
vitor <vi.lupinetti@hotmail.com.br>
2023-10-10 18:14:42 -0300

    Modelagem do agregado produto

---
+++ src/TechLanches/TechLanches.Domain/Aggregates/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechLanches.Core;
+using TechLanches.Domain.ValueObjects;
+
+namespace TechLanches.Domain.Aggregates
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        protected Produto() { }
+
+        protected Produto(string nome, string descricao, double preco, int categoriaId)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Preco = preco;
+            Categoria = CategoriaProduto.From(categoriaId);
+
+            Validar();
+        }
+
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public double Preco { get; private set; }
+        public CategoriaProduto Categoria { get; private set; }
+
+        private void Validar()
+        {
+            ArgumentNullException.ThrowIfNull(Nome);
+            if (Nome.Length > 100)
+                throw new DomainException("Nome deve possuir no máximo 100 caracteres.");
+
+            ArgumentNullException.ThrowIfNull(Descricao);
+            if (Descricao.Length > 300)
+                throw new DomainException("Descrição deve possuir no máximo 300 caracteres.");
+
+            ArgumentNullException.ThrowIfNull(Preco);
+            if (Preco <= 0)
+                throw new DomainException("Preço deve ser maior do que 0.");
+        }
+    }
+}
---
+++ src/TechLanches/TechLanches.Domain/ValueObjects/CategoriaProduto.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechLanches.Core;
+
+namespace TechLanches.Domain.ValueObjects
+{
+    public class CategoriaProduto:Enumeration
+    {
+        public static CategoriaProduto Lanche = new(1, nameof(Lanche));
+        public static CategoriaProduto Acompanhamento = new(2, nameof(Acompanhamento));
+        public static CategoriaProduto Bebida = new(3, nameof(Bebida));
+        public static CategoriaProduto Sobremesa = new(4, nameof(Sobremesa));
+        public CategoriaProduto(int id, string nome)
+        : base(id, nome)
+        {
+        }
+
+        public static IEnumerable<CategoriaProduto> List() =>
+       new[] { Lanche, Acompanhamento, Bebida, Sobremesa };
+
+        public static CategoriaProduto From(int id)
+        {
+            var categoria = List().SingleOrDefault(s => s.Id == id);
+
+            if (categoria == null)
+            {
+                throw new DomainException($"Possiveis valores para categoria: {string.Join(",", List().Select(s => $"{s.Id}-{s.Nome}"))}");
+            }
+
+            return categoria;
+        }
+    }
+}
