684a7c9353ba2228104b5b795a5994484d606319
Nicolas Soares <82896317+nsosoares@users.noreply.github.com>
2023-10-10 21:08:58 -0300

    Modelagem de pedido em andamento

---
+++ src/TechLanches/TechLanches.Core/Enumeration.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Reflection;
+
+namespace TechLanches.Core
+{
+    public abstract class Enumeration : IComparable
+    {
+        public string Nome { get; private set; }
+
+        public int Id { get; private set; }
+
+        protected Enumeration(int id, string nome) => (Id, Nome) = (id, nome);
+
+        public override string ToString() => Nome;
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration =>
+            typeof(T).GetFields(BindingFlags.Public |
+                                BindingFlags.Static |
+                                BindingFlags.DeclaredOnly)
+                        .Select(f => f.GetValue(null))
+                        .Cast<T>();
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Enumeration otherValue)
+            {
+                return false;
+            }
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public static int AbsoluteDifference(Enumeration firstValue, Enumeration secondValue)
+        {
+            var absoluteDifference = Math.Abs(firstValue.Id - secondValue.Id);
+            return absoluteDifference;
+        }
+
+        public static T FromValue<T>(int value) where T : Enumeration
+        {
+            var matchingItem = Parse<T, int>(value, "value", item => item.Id == value);
+            return matchingItem;
+        }
+
+        public static T FromDisplayName<T>(string displayName) where T : Enumeration
+        {
+            var matchingItem = Parse<T, string>(displayName, "display name", item => item.Nome == displayName);
+            return matchingItem;
+        }
+
+        private static T Parse<T, K>(K value, string description, Func<T, bool> predicate) where T : Enumeration
+        {
+            var matchingItem = GetAll<T>().FirstOrDefault(predicate);
+
+            if (matchingItem == null)
+                throw new InvalidOperationException($"'{value}' is not a valid {description} in {typeof(T)}");
+
+            return matchingItem;
+        }
+
+        public int CompareTo(object other) => Id.CompareTo(((Enumeration)other).Id);
+    }
+}
