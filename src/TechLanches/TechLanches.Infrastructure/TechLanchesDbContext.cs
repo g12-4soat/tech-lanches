4d4ed9e4ed9411afb28ef0662c783d07d7b36fba
Gabriel Lima <gabriellimagomes.14@hotmail.com>
2023-10-15 21:47:40 -0300

    movendo DataSeeder para classe separada

--- src/TechLanches/TechLanches.Infrastructure/TechLanchesDbContext.cs
+++ src/TechLanches/TechLanches.Infrastructure/TechLanchesDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
 using TechLanches.Core;
 using TechLanches.Domain.Entities;
 
@@ -24,30 +23,6 @@ namespace TechLanches.Infrastructure
             await base.SaveChangesAsync();
             
             //publicar eventos
-        }
-    }
-
-    public static class DataSeeder
-    {
-        public static void Seed(TechLanchesDbContext context)
-        {
-            ClientesSeed(context);
-        }
-
-        private static void ClientesSeed(TechLanchesDbContext context)
-        {
-            if (!context.Clientes.Any())
-            {
-                var countries = new List<Cliente>
-                {
-                    new Cliente("João Silva", "joao.silva@example.com", "02562905040"),
-                    new Cliente("Maria Santos", "maria.santos@example.com", "12687590070"),
-                    new Cliente("José Pereira", "jose.pereira@example.com", "18696905083")
-                };
-
-                context.AddRange(countries);
-                context.SaveChanges();
-            }
         }
     }
 }
