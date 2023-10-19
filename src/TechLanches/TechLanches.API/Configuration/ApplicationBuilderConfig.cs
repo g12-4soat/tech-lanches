ba10c68881e4bc2d5dcb4839613c6db3193959f6
vitor <vi.lupinetti@hotmail.com.br>
2023-10-18 22:48:45 -0300

    add middleware de erros para exceptions

---
+++ src/TechLanches/TechLanches.API/Configuration/ApplicationBuilderConfig.cs
@@ -0,0 +1,10 @@
+﻿using TechLanches.Infrastructure.Middlewares;
+
+namespace TechLanches.API.Configuration
+{
+    public static class ApplicationBuilderConfig
+    {
+        public static IApplicationBuilder AddGlobalErrorHandler(this IApplicationBuilder applicationBuilder)
+        => applicationBuilder.UseMiddleware<GlobalErrorHandlingMiddleware>();
+    }
+}
