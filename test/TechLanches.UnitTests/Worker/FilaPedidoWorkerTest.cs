1a5a5a36ec55cd18ec3ea735455a51c90764d2b4
vitor <vi.lupinetti@hotmail.com.br>
2023-10-22 21:26:37 -0300

    add testes unitários para fila pedidos worker e service

---
+++ test/TechLanches.UnitTests/Worker/FilaPedidoWorkerTest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using TechLanches.Adapter.FilaPedidos;
+using TechLanches.Application.Ports.Services;
+using TechLanches.Domain.Aggregates;
+using TechLanches.Domain.Enums;
+using TechLanches.Domain.ValueObjects;
+
+namespace TechLanches.UnitTests.Worker
+{
+    public class FilaPedidoWorkerTest
+    {
+        [Fact]
+        public async Task ExecuteAsync_ComProximoPedido_DeveProcessarPedido()
+        {
+            // Arrange
+            var mockFilaPedidoService = Substitute.For<IFilaPedidoService>();
+            mockFilaPedidoService.RetornarPrimeiroPedidoDaFila().Returns(new Pedido(1, new List<ItemPedido> { new ItemPedido(1, 1, 1) }));
+            var mockLogger = Substitute.For<ILogger<FilaPedidosHostedService>>();
+
+            var workerService = new FilaPedidosHostedService(mockLogger, mockFilaPedidoService);
+
+            // Act
+            await workerService.StartAsync(CancellationToken.None);
+            await Task.Delay(1000 * 21); // Aguardando um período para permitir que o método ExecuteAsync seja executado
+
+            // Assert
+            await mockFilaPedidoService.Received(1).RetornarPrimeiroPedidoDaFila();
+            await mockFilaPedidoService.Received(1).TrocarStatus(Arg.Any<Pedido>(), StatusPedido.PedidoEmPreparacao);
+            await mockFilaPedidoService.Received(1).TrocarStatus(Arg.Any<Pedido>(), StatusPedido.PedidoPronto);
+        }
+
+        [Fact]
+        public async Task ExecuteAsync_SemProximoPedido_NaoDeveProcessar()
+        {
+            // Arrange
+            var mockFilaPedidoService = Substitute.For<IFilaPedidoService>();
+            mockFilaPedidoService.RetornarPrimeiroPedidoDaFila().Returns(Task.FromResult<Pedido>(null));
+
+            var mockLogger = Substitute.For<ILogger<FilaPedidosHostedService>>();
+
+            var workerService = new FilaPedidosHostedService(mockLogger, mockFilaPedidoService);
+
+            // Act
+            await workerService.StartAsync(CancellationToken.None);
+            await Task.Delay(100); 
+
+            // Assert
+            await mockFilaPedidoService.Received(1).RetornarPrimeiroPedidoDaFila();
+            await mockFilaPedidoService.DidNotReceive().TrocarStatus(Arg.Any<Pedido>(), Arg.Any<StatusPedido>());
+        }
+      
+        [Fact]
+        public async Task ExecuteAsync_ComExcecao_NoCatch_DeveLogarErro()
+        {
+            // Arrange
+            var mockFilaPedidoService = Substitute.For<IFilaPedidoService>();
+            mockFilaPedidoService.RetornarPrimeiroPedidoDaFila().Throws(x => throw new Exception("Simulando erro"));
+
+            var mockLogger = Substitute.For<ILogger<FilaPedidosHostedService>>();
+
+            var workerService = new FilaPedidosHostedService(mockLogger, mockFilaPedidoService);
+
+            // Act
+            await workerService.StartAsync(CancellationToken.None);
+            await Task.Delay(100);
+
+            // Assert
+            await mockFilaPedidoService.Received(1).RetornarPrimeiroPedidoDaFila();
+            await mockFilaPedidoService.DidNotReceive().TrocarStatus(Arg.Any<Pedido>(), Arg.Any<StatusPedido>());
+        }
+
+    }
+}
