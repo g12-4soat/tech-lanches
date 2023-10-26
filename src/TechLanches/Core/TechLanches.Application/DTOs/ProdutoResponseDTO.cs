47de25bb3558e04be9213c79323a2b9842363e61
Deamorim <deni.barbosa.amori@gmail.com>
2023-10-25 22:52:40 -0300

    Documentação do swagger e ajuste no retorno dos endpoints.

--- src/TechLanches/Core/TechLanches.Application/DTOs/ProdutoResponseDTO.cs
+++ src/TechLanches/Core/TechLanches.Application/DTOs/ProdutoResponseDTO.cs
@@ -1,11 +1,38 @@
 ﻿namespace TechLanches.Application.DTOs
 {
+    /// <summary>
+    /// Schema utilizado para retorno de dados do produto.
+    /// </summary>
     public class ProdutoResponseDTO
     {
+        /// <summary>
+        /// Id do produto
+        /// </summary>
+        /// <example>1</example>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Nome do produto
+        /// </summary>
+        /// <example>X-Burguer</example>
         public string Nome { get; set; }
+
+        /// <summary>
+        /// Descrição do produto
+        /// </summary>
+        /// <example>Lanche com pão carne e queijo</example>
         public string Descricao { get;  set; }
+
+        /// <summary>
+        /// Preço do produto
+        /// </summary>
+        /// <example>25</example>
         public decimal Preco { get;  set; }
+
+        /// <summary>
+        /// Categoria do produto
+        /// </summary>
+        /// <example>Lanche</example>
         public string Categoria { get;  set; }
     }
 }
