6afaa8088c470087e909829918e35e9ad677db2d
Nicolas Soares <82896317+nsosoares@users.noreply.github.com>
2023-12-08 19:40:51 -0300

    Correção dos hpas

--- test/TechLanches.StressTests/hpa-test.js
+++ test/TechLanches.StressTests/hpa-test.js
@@ -1,13 +1,19 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+// export const options = {
+//   duration: '100s',
+//   vus: 300,
+// };
+
 export const options = {
-  duration: '100s',
-  vus: 300,
+  stages: [
+    { target: 800, duration: '30s' },
+    { target: 0, duration: '30s' },
+  ],
 };
 
 export default function () {
   let res = http.get('http://localhost:5050/api/pedidos');
   check(res, { 'status 200 OK': (r) => r.status === 200 });
-  sleep(1);
 }
