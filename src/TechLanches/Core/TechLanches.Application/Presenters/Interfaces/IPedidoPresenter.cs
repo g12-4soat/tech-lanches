de6d194807769ed4b54c83e05d9a96ba85f3e55d
nsosoares <nsosoares@gmail.com>
2024-01-21 14:21:12 -0300

    Presenter de pedido

---
+++ src/TechLanches/Core/TechLanches.Application/Presenters/Interfaces/IPedidoPresenter.cs
@@ -0,0 +1,11 @@
+﻿using TechLanches.Application.DTOs;
+using TechLanches.Domain.Aggregates;
+
+namespace TechLanches.Application.Presenters.Interfaces
+{
+    public interface IPedidoPresenter
+    {
+        PedidoResponseDTO ParaDto(Pedido entidade);
+        List<PedidoResponseDTO> ParaListaDto(List<Pedido> entidade);
+    }
+}
---
+++ src/TechLanches/Core/TechLanches.Application/Presenters/PedidoPresenter.cs
@@ -0,0 +1,20 @@
+﻿using Mapster;
+using TechLanches.Application.DTOs;
+using TechLanches.Application.Presenters.Interfaces;
+using TechLanches.Domain.Aggregates;
+
+namespace TechLanches.Application.Presenters
+{
+    public class PedidoPresenter : IPedidoPresenter
+    {
+        public PedidoResponseDTO ParaDto(Pedido entidade)
+        {
+            return entidade.Adapt<PedidoResponseDTO>();
+        }
+
+        public List<PedidoResponseDTO> ParaListaDto(List<Pedido> entidade)
+        {
+            return entidade.Adapt<List<PedidoResponseDTO>>();
+        }
+    }
+}
