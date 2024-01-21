c21332b308aafa7f7bdcdfeeab2d9affae80ccad
nsosoares <nsosoares@gmail.com>
2024-01-21 14:20:59 -0300

    Separação dos arquivos de PedidoDTO

---
+++ src/TechLanches/Core/TechLanches.Application/DTOs/PedidoResponseDTO.cs
@@ -0,0 +1,40 @@
+﻿namespace TechLanches.Application.DTOs
+{
+    /// <summary>
+    /// Schema utilizado para retorno de dados do pedido.
+    /// </summary>
+    public class PedidoResponseDTO
+    {
+        /// <summary>
+        /// Id do pedido
+        /// </summary>
+        /// <example>1</example>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Id do cliente
+        /// </summary>
+        /// <example>2</example>
+        public int? ClienteId { get; set; }
+
+        /// <summary>
+        /// Nome do cliente
+        /// </summary>
+        /// <example>Ana Luiza</example>
+        public string NomeCliente { get; set; }
+
+        /// <summary>
+        /// Status do pedido
+        /// </summary>
+        /// <example>PedidoCriado</example>
+        public string StatusPedido { get; set; }
+
+        /// <summary>
+        /// Valor total do pedido
+        /// </summary>
+        /// <example>12</example>
+        public decimal Valor { get; set; }
+
+        public List<ItemPedidoResponseDTO> ItensPedido { get; set; }
+    }
+}
