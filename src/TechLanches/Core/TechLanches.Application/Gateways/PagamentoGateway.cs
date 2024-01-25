44226436034910e8cf27794f569945e90673c333
nsosoares <nsosoares@gmail.com>
2024-01-25 06:53:52 -0300

    Gateway mercado pago

--- src/TechLanches/Core/TechLanches.Application/Gateways/PagamentoGateway.cs
+++ src/TechLanches/Core/TechLanches.Application/Gateways/PagamentoGateway.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
+using TechLanches.Adapter.ACL.Pagamento.QrCode.DTOs;
+using TechLanches.Adapter.ACL.Pagamento.QrCode.Provedores.MercadoPago;
 using TechLanches.Application.Gateways.Interfaces;
+using TechLanches.Application.Options;
 using TechLanches.Application.Ports.Repositories;
 using TechLanches.Domain.Aggregates;
 
@@ -12,10 +11,23 @@ namespace TechLanches.Application.Gateways
     public class PagamentoGateway : IPagamentoGateway
     {
         private readonly IPagamentoRepository _pagamentoRepository;
+        private readonly IMercadoPagoMockadoService _mercadoPagoMockadoService;
+        private readonly IMercadoPagoService _mercadoPagoService;
+        private readonly ApplicationOptions _applicationOptions;
+        private readonly bool _mockado;
 
-        public PagamentoGateway(IPagamentoRepository pagamentoRepository)
+        public PagamentoGateway(
+            IPagamentoRepository pagamentoRepository, 
+            IMercadoPagoMockadoService mercadoPagoMockadoService, 
+            IMercadoPagoService mercadoPagoService,
+            ApplicationOptions applicationOptions,
+            bool mockado)
         {
             _pagamentoRepository = pagamentoRepository;
+            _mercadoPagoMockadoService = mercadoPagoMockadoService;
+            _mercadoPagoService = mercadoPagoService;
+            _applicationOptions = applicationOptions;
+            _mockado = mockado;
         }
 
         public Task<Pagamento> BuscarPagamentoPorPedidoId(int pedidoId)
@@ -26,6 +38,28 @@ namespace TechLanches.Application.Gateways
         public Task<Pagamento> Cadastrar(Pagamento pagamento)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<PagamentoResponseACLDTO> ConsultarPagamentoMercadoPago(string pedidoComercial)
+        {
+            if (_mockado)
+            {
+                return await _mercadoPagoMockadoService.ConsultarPagamento(pedidoComercial);
+            }
+
+            return await _mercadoPagoService.ConsultarPagamento(pedidoComercial);
+        }
+
+        public async Task<string> GerarPagamentoEQrCodeMercadoPago(PedidoACLDTO pedidoMercadoPago)
+        {
+            var pedido = JsonSerializer.Serialize(pedidoMercadoPago);
+
+            if (_mockado)
+            {
+                return await _mercadoPagoMockadoService.GerarPagamentoEQrCode(pedido, _applicationOptions.UserId, _applicationOptions.PosId);
+            }
+
+            return await _mercadoPagoService.GerarPagamentoEQrCode(pedido, _applicationOptions.UserId, _applicationOptions.PosId);
         }
 
         public async Task CommitAsync()
